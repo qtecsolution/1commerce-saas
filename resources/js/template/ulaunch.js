8903ced932f03efeb02914bff76d6d71638a8cec
Muhaimin Shihab <talk.shihab@gmail.com>
2024-07-15 11:45:49 +0600

    add: vue.js to the ulaunch template

---
+++ resources/js/template/ulaunch.js
@@ -0,0 +1,19 @@
+import { createApp } from "vue";
+import axios from "axios";
+import Swal from 'sweetalert2';
+
+const app = createApp({
+    data() {
+        return {
+            
+        }
+    },
+    mounted() {
+
+    },
+    methods: {
+        
+    }
+});
+
+app.mount('#app');
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    // .js('resources/js/', 'public/assets/js/').vue()
+    .js('resources/js/template/ulaunch.js', 'public/js/ulaunch.js').vue()
     .sourceMaps();
