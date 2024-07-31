8dd5f8367c08da436d8bd401a315aacc3516f3d9
s1kopath <asaduzzaman.arob@gmail.com>
2024-07-31 12:03:10 +0600

    update: live preview

--- app/Http/Controllers/User/ShopController.php
+++ app/Http/Controllers/User/ShopController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\Shop;
+use App\Models\Template\UlaunchTemplate;
+use App\Models\Template\UserTemplate;
 use App\Models\UserTheme;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
@@ -12,6 +14,16 @@ class ShopController extends Controller
 {
     public function livePreview($slug)
     {
-        return 'Loading';
+        $userTemplate = UserTemplate::where('company_slug', $slug)->firstOrFail();
+        if ($userTemplate->template_id == 1) {
+            $ulaunch = UlaunchTemplate::with(['steps', 'features', 'testimonials'])
+                ->where('user_id', $userTemplate->user_id)
+                ->firstOrFail();
+            abort(404);
+
+            return view('template.live.ulaunch', compact('ulaunch'));
+        }
+
+        abort(404);
     }
 }
