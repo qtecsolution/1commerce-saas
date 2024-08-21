f29b0a79d11d3aa642071cc636b3f65cabd49254
s1kopath <asaduzzaman.arob@gmail.com>
2024-08-21 16:09:38 +0600

    merge: with aziz

--- database/seeders/UserTemplateSeeder.php
+++ database/seeders/UserTemplateSeeder.php
@@ -9,36 +9,36 @@ use Illuminate\Support\Str;
 use App\Http\Controllers\Template\CycleTemplateController;
 
 class UserTemplateSeeder extends Seeder
-{ 
+{
     /**
-    * Run the database seeds.
-    *
-    * @return void
-    */
-   public function run()
-   {
-    DB::table('user_templates')->insert([
-        //for cycle template
-        [
-            'template_id' => 3, // Make sure this ID exists in the templates table
-            'user_id' => 2, // Make sure this ID exists in the users table
-            'company_name' => 'Example Company Ltd.',
-            'company_slug' => Str::slug('Example Company'),
-            'fav_icon' => 'favicon.ico',
-            'company_logo' => null,
-            'product_name' => 'Example Product',
-            'shipping_cost_inside_dhaka' => 100.00,
-            'shipping_cost_outside_dhaka' => 200.00,
-            'product_price' => 15000.00,
-            'product_currency' => 'BDT',
-            'status' => 1,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ],
-        // Add more rows as needed
-    ]);
-    //for cycle template insert demo data in created user_template
-    $cycle = new CycleTemplateController();
-    $cycle->initialSetup(2);
-   }
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('user_templates')->insert([
+            //for cycle template
+            [
+                'template_id' => 3, // Make sure this ID exists in the templates table
+                'user_id' => 2, // Make sure this ID exists in the users table
+                'company_name' => 'Example Company Ltd.',
+                'company_slug' => Str::slug('Example Company'),
+                'fav_icon' => 'favicon.ico',
+                'company_logo' => null,
+                'product_name' => 'Example Product',
+                'shipping_cost_inside_dhaka' => 100.00,
+                'shipping_cost_outside_dhaka' => 200.00,
+                'product_price' => 15000.00,
+                'product_currency' => 'BDT',
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            // Add more rows as needed
+        ]);
+        //for cycle template insert demo data in created user_template
+        $cycle = new CycleTemplateController();
+        $cycle->initialSetup(2);
+    }
 }
