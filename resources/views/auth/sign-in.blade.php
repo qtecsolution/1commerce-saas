fe1a3ed46b63937d679c24c9e11e85ecbc988017
s1kopath <asaduzzaman.arob@gmail.com>
2024-08-22 18:37:29 +0600

    fix: sign in, sign up page design

--- resources/views/auth/sign-in.blade.php
+++ resources/views/auth/sign-in.blade.php
@@ -4,9 +4,9 @@
 @extends('layouts.guest')
 
 @section('page_content')
-    <div class="container-fluid p-h-0 p-v-20 bg full-height d-flex" style="background-image: url('assets/images/login.jpg')">
+    <div class="container-fluid p-h-0 p-v-20 bg full-height d-flex" style="background-image: url('assets/images/login.jpg');">
         <div class="d-flex flex-column justify-content-between w-100">
-            <div class="container d-flex h-100">
+            <div class="container d-flex h-100" style="padding-right: 0; padding-left: 30px;">
                 <div class="row align-items-center w-100">
                     <div class="col-md-7 col-lg-5 m-h-auto">
                         <div class="card shadow-lg">
@@ -40,12 +40,12 @@
                                         </div>
                                     </div>
                                     <div class="form-group">
-                                        <div class="d-flex align-items-center justify-content-between">
-                                            <span class="font-size-13 text-muted">
-                                                Don't have an account?
-                                                <a class="small" href="{{ route('sign_up') }}"> Sign-up</a>
-                                            </span>
-                                            <button class="btn btn-primary" type="submit">Sign-in</button>
+                                        <span class="font-size-13 text-muted">
+                                            Don't have an account?
+                                            <a class="small" href="{{ route('sign_up') }}"> Sign-up</a>
+                                        </span>
+                                        <div class="text-right mt-2">
+                                            <button class="btn btn-sm btn-primary" type="submit">Sign-in</button>
                                         </div>
                                     </div>
                                 </form>
